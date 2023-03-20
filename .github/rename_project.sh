4a7822af5018dd871309bfe0ec167cafc4914eb7
ivanmarinoff <ivanmarinoff@users.noreply.github.com>
2023-03-20 09:57:22 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="ivanmarinoff"
+original_name="remuneration"
+original_urlname="remuneration"
+original_description="Awesome remuneration created by ivanmarinoff"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv remuneration $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
